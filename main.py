fbd90d370ec7fa73b6ba71608076601f4cf2cf18
Ali Abdullah Nasiri <105828464+aliabdullahnasiri@users.noreply.github.com>
2024-07-22 19:59:10 +0430

    Update main.py

--- main.py
+++ main.py
@@ -1,50 +1,20 @@
-import logging
-import logging.handlers
-import os
+import urllib3
 
-import requests
+from hamster import Hamster
+from tokens import tokens
+from function import send_email
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-logger_file_handler = logging.handlers.RotatingFileHandler(
-    "status.log",
-    maxBytes=1024 * 1024,
-    backupCount=1,
-    encoding="utf8",
-)
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-logger_file_handler.setFormatter(formatter)
-logger.addHandler(logger_file_handler)
 
-try:
-    SOME_SECRET = os.environ["SOME_SECRET"]
-except KeyError:
-    SOME_SECRET = "Token not available!"
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-if __name__ == "__main__":
-    logger.info(f"Token value: {SOME_SECRET}")
+for token in tokens:
+    hamster = Hamster(token)
+    data = hamster.collect_passive_earn()
 
-    url = "https://sandbox.api.mailtrap.io/api/send/3034355"
-    headers = {
-        "Authorization": "Bearer 60258dfa6c4d18a7f92b3108cd33d5aa",
-        "Content-Type": "application/json"
-    }
-    data = {
-        "from": {
-            "email": "mailtrap@example.com",
-            "name": "Mailtrap Test"
-        },
-        "to": [
-            {
-                "email": "nasiri.aliabdullah@gmail.com"
-            }
-        ],
-        "subject": "You are awesome!",
-        "text": "Congrats for sending test email with Mailtrap!",
-        "category": "Integration Test"
-    }
-    
-    response = requests.post(url, headers=headers, json=data)
-    
-    print(response.status_code)
-    print(response.json())
+    string = ""
+    for key, value in data.items:
+        string += f"{key}: {value}\n"
+
+    string += "\n\n\n"
+
+    send_email("Hamster Kombat", string)
