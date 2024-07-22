c67ee9e77dc45290921eb4e2256a8153e4d30b96
Ali Abdullah Nasiri <105828464+aliabdullahnasiri@users.noreply.github.com>
2024-07-22 18:26:01 +0430

    Update main.py

--- main.py
+++ main.py
@@ -20,15 +20,31 @@ try:
     SOME_SECRET = os.environ["SOME_SECRET"]
 except KeyError:
     SOME_SECRET = "Token not available!"
-    #logger.info("Token not available!")
-    #raise
-
 
 if __name__ == "__main__":
     logger.info(f"Token value: {SOME_SECRET}")
 
-    r = requests.get('https://weather.talkpython.fm/api/weather/?city=Berlin&country=DE')
-    if r.status_code == 200:
-        data = r.json()
-        temperature = data["forecast"]["temp"]
-        logger.info(f'Weather in Berlin: {temperature}')
+    url = "https://sandbox.api.mailtrap.io/api/send/3034355"
+    headers = {
+        "Authorization": "Bearer 60258dfa6c4d18a7f92b3108cd33d5aa",
+        "Content-Type": "application/json"
+    }
+    data = {
+        "from": {
+            "email": "mailtrap@example.com",
+            "name": "Mailtrap Test"
+        },
+        "to": [
+            {
+                "email": "nasiri.aliabdullah@gmail.com"
+            }
+        ],
+        "subject": "You are awesome!",
+        "text": "Congrats for sending test email with Mailtrap!",
+        "category": "Integration Test"
+    }
+    
+    response = requests.post(url, headers=headers, json=data)
+    
+    print(response.status_code)
+    print(response.json())
